b2b4a8196dc7e1df3f3ccc778ae8ef47e5671ccd
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-10-24 23:49:35 -0700

    Refactored the code to correctly rearrange the command line inputs

--- rearrange.py
+++ rearrange.py
@@ -1,44 +1,23 @@
 import random
 import sys
 
-# print ('Number of arguments:', len(sys.argv), 'arguments.')
-# print ('Argument List:', str(sys.argv))
-
-# print(sys.argv[1:])
-
 
 def random_arrangement():
+    # Creates a list for the command line inputs starting from the 1 index
     inputs_list = sys.argv[1:]
+    # An empty list to append the command lines as strings
     result_list = []
-    for i in inputs_list:
-        rand_index = random.randint(0, len(inputs_list) -1)
-        list = inputs_list[rand_index]
-        result_list.append(list)
+    # shuffles the list
+    random.shuffle(inputs_list)
+
+    for command_line in inputs_list:
+        # Adds the indexs from the input_list to the empty result_list
+        result_list.append(command_line)
+        # Converts the list into strings
         str1 = ' '.join(result_list)
     return str1
-    # pass
 
 
 if __name__ == '__main__':
     arrange = random_arrangement()
     print(arrange)
-
-
-
-
-
-
-
-#
-# quotes = ("It's just a flesh wound.",
-#           "He's not the Messiah. He's a very naughty boy!",
-#           "THIS IS AN EX-PARROT!!")
-#
-#
-# def random_python_quote():
-#     rand_index = random.randint(0, len(quotes) - 1)
-#     return quotes[rand_index]
-#
-# if __name__ == '__main__':
-#     quote = random_python_quote()
-#     print (quote)
